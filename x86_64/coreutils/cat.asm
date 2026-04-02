348dfe374183907c9c2a4023e7240eb778a73a13
asmfs <eulfs@proton.me>
2026-04-02 10:24:47 +0200

    Update cat.asm

--- x86_64/coreutils/cat.asm
+++ x86_64/coreutils/cat.asm
@@ -1,17 +1,45 @@
 section .bss
-input resb 4096
+file resb 512
+section .data
+cmd_e db "exit"
+newl db 10
 section .text
 global _start
 _start:
-.rep:
+.src:
 mov rax, 0
 mov rdi, 0
-mov rsi, input
-mov rdx, 4096
+mov rsi, file
+mov rdx, 55
+syscall
+dec rax
+mov byte[file+rax], 0
+mov eax, [file]
+mov ebx, [cmd_e]
+cmp eax, ebx
+je .cmd_e_src
+mov rax, 2
+mov rdi, file
+mov rsi, 0
+mov rdx, 0
+syscall
+mov rdi, rax
+mov rax, 0
+mov rsi, file
+mov rdx, 512
 syscall
 mov rdx, rax
 mov rax, 1
 mov rdi, 1
-mov rsi, input
+mov rsi, file
 syscall
-jmp .rep
+mov rax, 1
+mov rdi, 1
+mov rsi, newl
+mov rdx, 1
+syscall
+jmp .src
+.cmd_e_src:
+mov rax, 60
+mov rdi, 1
+syscall
