25167c4c57e06aff78f0ccc3c33648e5ae402a03
asmfs <eulfs@proton.me>
2026-04-02 15:30:11 +0200

    Update mkdir.asm

--- x86_64/coreutils/mkdir.asm
+++ x86_64/coreutils/mkdir.asm
@@ -5,6 +5,8 @@ nerrlog db "xalu: directory created;", 10
 nerrloglen equ $-nerrlog
 errlog db "xalu: directory not created;", 10
 errloglen equ $-errlog
+null_log db "xalu: directory cannot be null;", 10
+null_loglen equ $-null_log
 section .text
 global _start
 _start:
@@ -16,10 +18,8 @@ mov rdx, 255
 syscall
 dec rax
 mov byte[nwdir+rax], 0
-mov r8d, [nwdir]
-cmp r8d, "exit"
-cmp eax, ebx
-je .exit
+cmp [nwdir], 0x00
+je .errnull
 mov rax, 83
 mov rdi, nwdir
 mov rsi, 0o755
@@ -40,7 +40,10 @@ mov rsi, errlog
 mov rdx, errloglen
 syscall
 jmp .mkdir
-.exit:
-mov rax, 60
+.errnull:
+mov rax, 1
 mov rdi, 1
+mov rsi, null_log
+mov rdx, null_loglen
 syscall
+jmp .mkdir
